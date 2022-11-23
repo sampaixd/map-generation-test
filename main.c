46262b875dc4c849197993fb08e31295bcce20f4
sampaixd <samuelbroman03@gmail.com>
2022-11-23 15:51:56 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,117 @@
+#include "raylib.h"
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+const int screenHeight = 720;
+const int screenWidth = 1280;
+const int targetFPS = 60;
+
+typedef struct mapSection_t
+{
+    Vector2 startPos;
+    Vector2 endPos;
+    struct mapSection_t *splitMapSections;
+} mapSection_t;
+
+void FreeBSPMap(int iterationCount, int desiredIterations, mapSection_t *mapSection)
+{
+    if (iterationCount < desiredIterations)
+    {
+        iterationCount++;
+        FreeBSPMap(iterationCount, desiredIterations, (mapSection->splitMapSections));
+        FreeBSPMap(iterationCount, desiredIterations, (mapSection->splitMapSections + 1));
+    }
+}
+
+void DrawBSPMapSections(int iterationCount, int desiredIterations, mapSection_t mapSection)
+{
+    // printf("draw iteration: %d\n", iterationCount);
+    //  getchar();
+    //  fflush(stdin);
+    DrawLineV(mapSection.splitMapSections[0].endPos, mapSection.splitMapSections[1].startPos, RED);
+    if (iterationCount >= desiredIterations)
+    {
+    }
+    else
+    {
+        iterationCount++;
+
+        DrawBSPMapSections(iterationCount, desiredIterations, mapSection.splitMapSections[0]);
+        // puts("wee");
+        DrawBSPMapSections(iterationCount, desiredIterations, mapSection.splitMapSections[1]);
+        // puts("wee 2");
+    }
+}
+
+void GenerateBSPMapSections(int iterationCount, int desiredIterations, mapSection_t *mapSection)
+{
+    mapSection->splitMapSections = (mapSection_t *)calloc(2, sizeof(mapSection_t));
+    printf("gen iteration: %d\n", iterationCount);
+
+    if (rand() % 2)
+    {
+        puts("split in x");
+        // splits in random place with atleast a 20% margin to the top/bottom
+        float xSplit = (((rand() % (int)(mapSection->endPos.x * 0.6)) * 100) / 100) + (mapSection->endPos.x * 0.2);
+        // float xSplit = (mapSection->startPos.x + mapSection->endPos.x) / 2;
+        mapSection->splitMapSections[0] = (mapSection_t){
+            .startPos = (Vector2){mapSection->startPos.x, mapSection->startPos.y},
+            .endPos = (Vector2){xSplit, mapSection->endPos.y}};
+
+        mapSection->splitMapSections[1] = (mapSection_t){
+            .startPos = (Vector2){xSplit, mapSection->startPos.y},
+            .endPos = (Vector2){mapSection->endPos.x, mapSection->endPos.y}};
+    }
+    else
+    {
+        puts("split in y");
+        // splits in random place with atleast a 20% margin to the left/right
+        // float ySplit = (((rand() % (int)(mapSection->endPos.y * 0.6)) * 100) / 100) + (mapSection->endPos.y * 0.2);
+        float ySplit = (mapSection->startPos.y + mapSection->endPos.y) / 2;
+        mapSection->splitMapSections[0] = (mapSection_t){
+            .startPos = (Vector2){mapSection->startPos.x, mapSection->startPos.y},
+            .endPos = (Vector2){mapSection->endPos.x, ySplit}};
+
+        mapSection->splitMapSections[1] = (mapSection_t){
+            .startPos = (Vector2){mapSection->startPos.x, ySplit},
+            .endPos = (Vector2){mapSection->endPos.x, mapSection->endPos.y}};
+    }
+    if (iterationCount >= desiredIterations)
+    {
+        printf("all iterations complete: iterations: %d, desired iterations: %d\n",
+               iterationCount, desiredIterations);
+        // return;
+    }
+    else
+    {
+        iterationCount++;
+        GenerateBSPMapSections(iterationCount, desiredIterations, (mapSection->splitMapSections));
+        GenerateBSPMapSections(iterationCount, desiredIterations, (mapSection->splitMapSections + 1));
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+    InitWindow(screenWidth, screenHeight, "map generation test");
+    SetTargetFPS(targetFPS);
+
+    int desiredIterations = 4;
+    mapSection_t map = (mapSection_t){
+        .startPos = (Vector2){0, 0},
+        .endPos = (Vector2){screenWidth, screenHeight},
+        .splitMapSections = NULL};
+
+    GenerateBSPMapSections(0, desiredIterations, &map);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawBSPMapSections(0, desiredIterations, map);
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
