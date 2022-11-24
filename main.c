9b6644611adbf5c05dca546c35a4bce622dd8471
sampaixd <samuelbroman03@gmail.com>
2022-11-24 14:44:39 +0100

    feat added room generation, not fully functional

--- main.c
+++ main.c
@@ -4,54 +4,119 @@
 #include <stdlib.h>
 #include <stdio.h>
 // TODO make mapSectionMargin work to modify room sizes, add rooms and connect them
-const int screenHeight = 720;
-const int screenWidth = 1280;
+const int screenHeight = 1080;
+const int screenWidth = 1920;
 const int targetFPS = 60;
-const float mapSectionMargin = 0.4; // how many % magin from other map sections (1 = 100%)
-const int iterations = 3;           // iterations for the generator
+const float mapSectionMarginPercentage = 0.4; // how many % magin from other map sections (1 = 100%)
+const int iterations = 3;                     // iterations for the generator
 const int xySplitRandomizer = 8;
-int xySplitRandomizerThreshold = xySplitRandomizer / 2; // will get bigger/smaller depending on what the previous value were, this is used to avoid too many x/y slices happening after one another
-typedef struct mapSection_t
+int xySplitRandomizerThreshold = xySplitRandomizer / 2; // will get bigger/smaller depending on the previous split, this is used to avoid too many x/y slices happening after one another
+const float roomMarginPercentage = 0.1;                 // 1% percision, will be rounded afterwards
+const float roomMinSizePercentage = 0.1;                // 1% percision, will be rounded afterwards
+
+typedef struct rect_t
 {
     Vector2 startPos;
     Vector2 endPos;
+} rect_t;
+
+typedef struct mapSection_t
+{
+    rect_t area;
+    rect_t room;
+    rect_t splitMapSecionsCorridor;
     struct mapSection_t *splitMapSections;
 } mapSection_t;
 
 void FreeBSPMap(int iterationCount, int desiredIterations, mapSection_t *mapSection)
 {
-    if (iterationCount < desiredIterations)
+    if (iterationCount <= desiredIterations)
     {
         iterationCount++;
         FreeBSPMap(iterationCount, desiredIterations, (mapSection->splitMapSections));
         FreeBSPMap(iterationCount, desiredIterations, (mapSection->splitMapSections + 1));
+        free(mapSection->splitMapSections);
     }
+    else
+    {
+        free(mapSection->splitMapSections);
+    }
+}
+
+Vector2 GetRectSize(rect_t rect)
+{
+    return (Vector2) { rect.endPos.x - rect.startPos.x , rect.endPos.y - rect.startPos.y};
+}
+
+void DrawRoom(rect_t room)
+{
+    Vector2 size = GetRectSize(room);
+    // puts("drawing room");
+    float textPosX = room.startPos.x + ((room.endPos.x - room.startPos.x) / 2);
+    float textPosY = room.startPos.y + ((room.endPos.y - room.startPos.y) / 2);
+    DrawRectangleV(room.startPos, size, BLUE);
+    DrawText(TextFormat("start pos: %.2f, %.2f\n"
+                        "end pos:   %.2f, %.2f\n",
+                        room.startPos.x, room.startPos.y,
+                        room.endPos.x, room.endPos.y),
+             textPosX, textPosY, 20, GREEN);
 }
 
 void DrawBSPMapSections(int iterationCount, int desiredIterations, mapSection_t mapSection, Color color)
 {
-    // printf("draw iteration: %d\n", iterationCount);
-    //  getchar();
-    //  fflush(stdin);
-    DrawLineV(mapSection.splitMapSections[0].endPos, mapSection.splitMapSections[1].startPos, color);
-    /*color = (Color){
-        .a = color.a + 1,
-        .b = color.b + 1,
-        .g = color.g + 1,
-        .r = color.r + 1
-    };*/
+
     if (iterationCount >= desiredIterations)
     {
+        DrawRoom(mapSection.splitMapSections[0].room);
+        DrawRoom(mapSection.splitMapSections[1].room);
     }
     else
     {
         iterationCount++;
 
         DrawBSPMapSections(iterationCount, desiredIterations, mapSection.splitMapSections[0], color);
-        // puts("wee");
+
         DrawBSPMapSections(iterationCount, desiredIterations, mapSection.splitMapSections[1], color);
-        // puts("wee 2");
     }
+    DrawLineV(mapSection.splitMapSections[0].area.endPos, mapSection.splitMapSections[1].area.startPos, color);
+    DrawText(TextFormat("%.2f, %.2f", mapSection.splitMapSections[0].area.endPos.x, mapSection.splitMapSections[0].area.endPos.y),
+             mapSection.splitMapSections[0].area.endPos.x,
+             mapSection.splitMapSections[0].area.endPos.y, 10, LIME);
+    DrawText(TextFormat("%.2f, %.2f", mapSection.splitMapSections[1].area.startPos.x, mapSection.splitMapSections[1].area.startPos.y),
+             mapSection.splitMapSections[1].area.startPos.x,
+             mapSection.splitMapSections[1].area.startPos.y, 10, LIME);
+}
+
+void GenerateRoom(rect_t *room, rect_t area)
+{
+    puts("generating room");
+    float xWidth = area.endPos.x - area.startPos.x;
+    float yWidth = area.endPos.y - area.startPos.y;
+    Vector2 startPos = {
+        // base value is beginning of the section + margin, then adds a random sum between 0 and the map section width - margin and min size (in order to make sure there will always be a min size available)
+        area.startPos.x + (xWidth * roomMarginPercentage) + xWidth * ((rand() % (int)(100 - (roomMarginPercentage * 100) - (roomMinSizePercentage * 100))) / 100),
+        area.startPos.y + (yWidth * roomMarginPercentage) + yWidth * ((rand() % (int)(100 - (roomMarginPercentage * 100) - (roomMinSizePercentage * 100))) / 100)};
+    Vector2 endPos = {
+        // base value is map section width * min size, then adds a random value between 0 and the distance from the startpos of the room to the end of the map section - margin
+        startPos.x + (xWidth * roomMinSizePercentage) + (xWidth * ((float)(rand() % (int)(100 - (((startPos.x - area.startPos.x) / xWidth) * 100) - (roomMarginPercentage * 100))) / 100)),
+        startPos.y + (yWidth * roomMinSizePercentage) + (yWidth * ((float)(rand() % (int)(100 - (((startPos.y - area.startPos.y) / yWidth) * 100) - (roomMarginPercentage * 100))) / 100))};
+    printf("map section start pos: %.2f, %.2f\n"
+           "map section end pos:   %.2f, %.2f\n"
+           "map section width:     %.2f, %.2f\n"
+           //"test:                  %.17f\n"
+           "room: \n"
+           "start pos: %.2f, %.2f\n"
+           "end pos:   %.2f, %.2f\n",
+           area.startPos.x, area.startPos.y,
+           area.endPos.x, area.endPos.y,
+           xWidth, yWidth,
+           //(startPos.x - area.startPos.x) / xWidth,
+           // xWidth * ((float)(rand() % (int)(100 - (roomMarginPercentage * 100) - (roomMinSizePercentage * 100))) / 100),
+           startPos.x, startPos.y,
+           endPos.x, endPos.y);
+    *room = (rect_t){
+        .startPos = startPos,
+        .endPos = endPos};
 }
 
 void GenerateBSPMapSections(int iterationCount, int desiredIterations, mapSection_t *mapSection)
@@ -64,40 +129,42 @@ void GenerateBSPMapSections(int iterationCount, int desiredIterations, mapSectio
         xySplitRandomizerThreshold++;
         puts("split in x");
         // splits in random place with atleast a 20% margin to the top/bottom
-        float xWidth = mapSection->endPos.x - mapSection->startPos.x;
-        float xSplit = (((rand() % (int)(xWidth * (1 - (mapSectionMargin * 2)))) * 100) / 100) + mapSection->startPos.x + (xWidth * mapSectionMargin);
-        // float xSplit = (((rand() % (int)((xWidth) * (1 - (0.8)))) * 100) / 100) + mapSection->startPos.x + ((xWidth) * 0.4);
-        //  float xSplit = (mapSection->startPos.x + mapSection->endPos.x) / 2;
+        float xWidth = mapSection->area.endPos.x - mapSection->area.startPos.x;
+        float xSplit = (((rand() % (int)(xWidth * (1 - (mapSectionMarginPercentage * 2)))) * 100) / 100) + mapSection->area.startPos.x + (xWidth * mapSectionMarginPercentage);
+        // float xSplit = (((rand() % (int)((xWidth) * (1 - (0.8)))) * 100) / 100) + mapSection->area.startPos.x + ((xWidth) * 0.4);
+        //  float xSplit = (mapSection->area.startPos.x + mapSection->area.endPos.x) / 2;
         mapSection->splitMapSections[0] = (mapSection_t){
-            .startPos = (Vector2){mapSection->startPos.x, mapSection->startPos.y},
-            .endPos = (Vector2){xSplit, mapSection->endPos.y}};
+            .area.startPos = (Vector2){mapSection->area.startPos.x, mapSection->area.startPos.y},
+            .area.endPos = (Vector2){xSplit, mapSection->area.endPos.y}};
 
         mapSection->splitMapSections[1] = (mapSection_t){
-            .startPos = (Vector2){xSplit, mapSection->startPos.y},
-            .endPos = (Vector2){mapSection->endPos.x, mapSection->endPos.y}};
+            .area.startPos = (Vector2){xSplit, mapSection->area.startPos.y},
+            .area.endPos = (Vector2){mapSection->area.endPos.x, mapSection->area.endPos.y}};
     }
     else
     {
         xySplitRandomizerThreshold--;
         puts("split in y");
         // splits in random place with atleast a 20% margin to the left/right
-        float yWidth = mapSection->endPos.y - mapSection->startPos.y;
-        float ySplit = (((rand() % (int)(yWidth * (1 - (mapSectionMargin * 2)))) * 100) / 100) + mapSection->startPos.y + (yWidth * mapSectionMargin);
-        // float ySplit = (((rand() % (int)((yWidth) * (1 - (0.8)))) * 100) / 100) + mapSection->startPos.y + ((yWidth) * 0.4);
+        float yWidth = mapSection->area.endPos.y - mapSection->area.startPos.y;
+        float ySplit = (((rand() % (int)(yWidth * (1 - (mapSectionMarginPercentage * 2)))) * 100) / 100) + mapSection->area.startPos.y + (yWidth * mapSectionMarginPercentage);
+        // float ySplit = (((rand() % (int)((yWidth) * (1 - (0.8)))) * 100) / 100) + mapSection->area.startPos.y + ((yWidth) * 0.4);
 
-        // float ySplit = (mapSection->startPos.y + mapSection->endPos.y) / 2;
+        // float ySplit = (mapSection->area.startPos.y + mapSection->area.endPos.y) / 2;
         mapSection->splitMapSections[0] = (mapSection_t){
-            .startPos = (Vector2){mapSection->startPos.x, mapSection->startPos.y},
-            .endPos = (Vector2){mapSection->endPos.x, ySplit}};
+            .area.startPos = (Vector2){mapSection->area.startPos.x, mapSection->area.startPos.y},
+            .area.endPos = (Vector2){mapSection->area.endPos.x, ySplit}};
 
         mapSection->splitMapSections[1] = (mapSection_t){
-            .startPos = (Vector2){mapSection->startPos.x, ySplit},
-            .endPos = (Vector2){mapSection->endPos.x, mapSection->endPos.y}};
+            .area.startPos = (Vector2){mapSection->area.startPos.x, ySplit},
+            .area.endPos = (Vector2){mapSection->area.endPos.x, mapSection->area.endPos.y}};
     }
     if (iterationCount >= desiredIterations)
     {
         printf("all iterations complete: iterations: %d, desired iterations: %d\n",
                iterationCount, desiredIterations);
+        GenerateRoom(&mapSection->splitMapSections[0].room, mapSection->splitMapSections[0].area);
+        GenerateRoom(&mapSection->splitMapSections[1].room, mapSection->splitMapSections[1].area);
         // return;
     }
     else
@@ -115,8 +182,8 @@ int main()
     SetTargetFPS(targetFPS);
 
     mapSection_t map = (mapSection_t){
-        .startPos = (Vector2){0, 0},
-        .endPos = (Vector2){screenWidth, screenHeight},
+        .area.startPos = (Vector2){0, 0},
+        .area.endPos = (Vector2){screenWidth, screenHeight},
         .splitMapSections = NULL};
 
     GenerateBSPMapSections(0, iterations, &map);
